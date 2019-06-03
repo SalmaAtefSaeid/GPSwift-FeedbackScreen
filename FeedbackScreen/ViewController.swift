34bf2fb0e88d0fd63f24a82b87dc4627a529dfde
Salma Atef <salma.atef.ebrahim@gmail.com>
2019-06-03 17:07:13 +0200

    Initial commit

--- FeedbackScreen/ViewController.swift
+++ FeedbackScreen/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var feedbackStack: UIStackView!
+    @IBOutlet weak var commentTxtView: UITextView!
+    var feedbackSelected = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        commentTxtView.delegate = self
+        commentTxtView.text = "Tell us your comment"
+        commentTxtView.textColor = UIColor.lightGray
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
     }
-
-
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Tell us your comment"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    @IBAction func goodFeedbackBtn(_ sender: UIButton) {
+        if sender.tintColor == UIColor.black{
+            deselectAllButtons()
+            feedbackSelected = true
+            sender.titleLabel?.font = UIFont(name: (sender.titleLabel?.font.fontName)!, size: 100)
+            sender.tintColor = UIColor.white
+        }else{
+            feedbackSelected = false
+            sender.titleLabel?.font = UIFont(name: (sender.titleLabel?.font.fontName)!, size: 60)
+            sender.tintColor = UIColor.black
+        }
+    }
+    @IBAction func badFeedbackBtn(_ sender: UIButton) {
+        if sender.tintColor == UIColor.black{
+            deselectAllButtons()
+            feedbackSelected = true
+            sender.titleLabel?.font = UIFont(name: (sender.titleLabel?.font.fontName)!, size: 100)
+            sender.tintColor = UIColor.white
+        }else{
+            feedbackSelected = false
+            sender.titleLabel?.font = UIFont(name: (sender.titleLabel?.font.fontName)!, size: 60)
+            sender.tintColor = UIColor.black
+        }
+    }
+    
+    @IBAction func neutralFeedbackBtn(_ sender: UIButton) {
+        if sender.tintColor == UIColor.black{
+            deselectAllButtons()
+            feedbackSelected = true
+            sender.titleLabel?.font = UIFont(name: (sender.titleLabel?.font.fontName)!, size: 100)
+            sender.tintColor = UIColor.white
+        }else{
+            feedbackSelected = false
+            sender.titleLabel?.font = UIFont(name: (sender.titleLabel?.font.fontName)!, size: 60)
+            sender.tintColor = UIColor.black
+        }
+    }
+    @IBAction func rateBtn(_ sender: UIButton) {
+    }
+    func deselectAllButtons(){
+        for subView in feedbackStack.subviews
+        {
+            if let button = subView as? UIButton{
+                feedbackSelected = false
+                button.titleLabel?.font = UIFont(name: (button.titleLabel?.font.fontName)!, size: 60)
+                button.tintColor = UIColor.black
+            }
+        }
+    }
 }
 
